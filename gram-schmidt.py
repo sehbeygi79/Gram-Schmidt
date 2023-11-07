940b3905c6c3bc730ba15165627b356e389afed9
sehbeygi79 <sehbeygi79@gmail.com>
2023-11-07 17:22:35 +0330

    sort_rows and print_matrix added

--- gram-schmidt.py
+++ gram-schmidt.py
@@ -13,7 +13,16 @@ def gram_schmidt(vectors):
     vectors /= row_norms[:, np.newaxis]
     return vectors
 
+def sort_rows(matrix, ascending=True):
+    order = 1 if ascending else -1
+    sorted_index = [order * matrix[:, i] for i in range(matrix.shape[1]-1, -1, -1)]
+    return matrix[np.lexsort(sorted_index)]
 
+def print_matrix(matrix):
+    for _, row in enumerate(matrix):
+        for elem in row:
+            print(f'{elem:.3f} ', end='')
+        print()
 
 
 n, m = list(map(int, input().split()))
@@ -21,6 +30,7 @@ A = [list(map(int, input().split())) for _ in range(n)]
 
 vectors = np.asarray(A, dtype=float)
 orthonormal_vectors = gram_schmidt(vectors) 
+print_matrix(sort_rows(orthonormal_vectors, ascending=False))
 
         
     
